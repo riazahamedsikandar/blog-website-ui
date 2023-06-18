87b9f4ca6b9c4fca6c9ffd9ce40da34505508d9a
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-06-18 17:09:34 +0530

    blog

---
+++ blog/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBrQstD8cu6ybAl8-6JxoUHccim2Hl58SE",
+    authDomain: "blog-ui-a2d53.firebaseapp.com",
+    projectId: "blog-ui-a2d53",
+    storageBucket: "blog-ui-a2d53.appspot.com",
+    messagingSenderId: "608346446935",
+    appId: "1:608346446935:web:9a1586b34bcca33bc91712",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ blog/src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+    return ( <
+        div >
+        <
+        img src = "/images/404.jpg"
+        alt = "Page not found" / >
+        <
+        /div>
+    );
+};
+
+export default NotFound;
