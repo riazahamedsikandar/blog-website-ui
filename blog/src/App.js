da94cba4448f8d0595e35ffd6961ef119174068f
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-06-18 19:57:12 +0530

    app

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./style.scss";
+import "./media-query.css";
+import Home from "./pages/Home";
 import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Header from "./components/Header";
-import ScrollToTop from "./components/ScrollToTop";
-import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import AddEditBlog from "./pages/AddEditBlog";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
 import Auth from "./pages/Auth";
-import TagBlog from "./pages/TagBlog";
-import CategoryBlog from "./pages/CategoryBlog";
-import Blogs from "./pages/Blogs";
 import { auth } from "./firebase";
 import { signOut } from "firebase/auth";
-
-import "./App.css";
-import "./style.scss";
-import "./media-query.css";
+import TagBlog from "./pages/TagBlog";
+import CategoryBlog from "./pages/CategoryBlog";
+import ScrollToTop from "./components/ScrollToTop";
+import Blogs from "./pages/Blogs";
 
 function App() {
     const [active, setActive] = useState("home");
     const [user, setUser] = useState(null);
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -36,67 +36,63 @@ function App() {
     }, []);
 
     const handleLogout = () => {
-        signOut(auth)
-            .then(() => {
-                setUser(null);
-                setActive("login");
-                navigate("/auth");
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
+        signOut(auth).then(() => {
+            setUser(null);
+            setActive("login");
+            navigate("/auth");
+        });
     };
 
     return ( <
-            div className = "App" >
-            <
-            Header setActive = { setActive }
-            active = { active }
-            user = { user }
-            handleLogout = { handleLogout }
-            /> <
-            ScrollToTop / >
-            <
-            ToastContainer position = "top-center" / >
-            <
             Routes >
             <
             Route path = "/"
             element = { < Home setActive = { setActive }
                 active = { active }
                 user = { user }
-                />} / >
+                />} /
+                >
                 <
                 Route
                 path = "/search"
                 element = { < Home setActive = { setActive }
+                    active = { active }
                     user = { user }
-                    />} / >
+                    />} /
+                    >
                     <
                     Route
                     path = "/detail/:id"
                     element = { < Detail setActive = { setActive }
                         user = { user }
-                        />} / >
+                        />} /
+                        >
                         <
                         Route
                         path = "/create"
                         element = {
-                            user ? .uid ? < AddEditBlog user = { user }
-                            /> : <Navigate to="/
-                            " />
+                            (user) => {
+                                if (user ? .uid) {
+                                    return <AddEditBlog user = { user }
+                                    />;
+                                } else {
+                                    return <Navigate to = "/" / > ;
+                                }
+                            }
                         }
                         /> <
                         Route
                         path = "/update/:id"
                         element = {
-                            user ? .uid ? ( <
-                                AddEditBlog user = { user }
-                                setActive = { setActive }
-                                />
-                            ) : ( <
-                                Navigate to = "/" / >
-                            )
+                            (user) => {
+                                if (user ? .uid) {
+                                    return <AddEditBlog user = { user }
+                                    setActive = { setActive }
+                                    />;
+                                } else {
+                                    return <Navigate to = "/" / > ;
+                                }
+                            }
                         }
                         /> <
                         Route path = "/blogs"
@@ -110,7 +106,8 @@ function App() {
                                 Route
                                 path = "/category/:category"
                                 element = { < CategoryBlog setActive = { setActive }
-                                    />} / >
+                                    />} /
+                                    >
                                     <
                                     Route path = "/about"
                                     element = { < About / > }
@@ -119,13 +116,13 @@ function App() {
                                     path = "/auth"
                                     element = { < Auth setActive = { setActive }
                                         setUser = { setUser }
-                                        />} / >
+                                        />} /
+                                        >
                                         <
                                         Route path = "*"
                                         element = { < NotFound / > }
-                                        /> < /
-                                        Routes > <
-                                        /div>
+                                        /> <
+                                        /Routes>
                                     );
                                 }
 
