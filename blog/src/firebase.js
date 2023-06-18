017f9c82365e41e61d9adef691a21a1a362e806e
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-06-18 18:19:55 +0530

    firebase

--- blog/src/firebase.js
+++ blog/src/firebase.js
@@ -1,8 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBrQstD8cu6ybAl8-6JxoUHccim2Hl58SE",
@@ -12,6 +11,9 @@ const firebaseConfig = {
     messagingSenderId: "608346446935",
     appId: "1:608346446935:web:9a1586b34bcca33bc91712",
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
