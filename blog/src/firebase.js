1557eda1b566e1662452296ec2f8e25be236b06f
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-07-01 23:42:55 +0530

    img firebase

--- blog/src/firebase.js
+++ blog/src/firebase.js
@@ -2,15 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
     apiKey: "AIzaSyBrQstD8cu6ybAl8-6JxoUHccim2Hl58SE",
     authDomain: "blog-ui-a2d53.firebaseapp.com",
     projectId: "blog-ui-a2d53",
     storageBucket: "blog-ui-a2d53.appspot.com",
     messagingSenderId: "608346446935",
-    appId: "1:608346446935:web:9a1586b34bcca33bc91712",
+    appId: "1:608346446935:web:9a1586b34bcca33bc91712"
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
